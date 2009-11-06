1921351229eae0bbca62835c683a61c108fffa27
Andy Gregorowicz <andy@mitre.org>
2009-11-06 10:45:28 -0500

    Got some basic tests set up for the index

--- test/root_test.rb
+++ test/root_test.rb
@@ -2,16 +2,33 @@ require 'test/test_helper'
 
 class RootTest < HDataTest
   context "The root of an hData Record" do
-    should "return a response to a get request" do
+    should "return a response to a GET request" do
       get '/'
       assert last_response.ok?      
     end
     
-    should "not allow a put" do
+    should "not allow a PUT" do
       put '/'
-      assert !last_response.ok?
-      assert_equal 405, last_response.status      
+      assert_equal 405, last_response.status
     end
     
+    context "when receiving a POST" do
+      should "not allow an incomplete request" do
+        post '/', {:type => 'extension'}
+        assert_equal 400, last_response.status
+      end
+      
+      should "allow the registration of a new extension" do
+        post '/', {:type => 'extension', :typeId => 'http://projecthdata.org/hdata/schemas/2009/06/allergy', 
+                   :requirement => 'mandatory'}
+        assert_equal 201, last_response.status
+      end
+      
+      should "allow the creation of a new section" do
+        post '/', {:type => 'section', :typeId => 'http://projecthdata.org/hdata/schemas/2009/06/allergy', 
+                   :path => 'allergies', :name => 'Allergies'}
+        assert_equal 201, last_response.status
+      end
+    end
   end
 end
